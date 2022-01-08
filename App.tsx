0a227fac4daca0f3803215d79952c867cdbf4f34
MoizAsif <moiz.asif@xynotech.com>
2022-01-08 14:34:27 +0500

    Setting up redux

--- App.tsx
+++ App.tsx
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import SwitchNavigator from './navigation/LoginNavigator'
+import  ThunkMiddleware  from 'redux-thunk';
+import reducer from './reducers/index';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+
+const middleware = applyMiddleware(ThunkMiddleware);
+const store = createStore(reducer, middleware);
 
 export default function App() {
   return (
+    <Provider store={store}>
     <SwitchNavigator />
+    </Provider>
   );
 }
 
---
+++ reducers/index.tsx
@@ -0,0 +1,15 @@
+import { combineReducers } from "redux";
+
+
+const user = (state= {}, action) => {
+     switch(action.type){
+            default:
+                return state
+     }
+}
+
+const rootReducer = combineReducers({
+     user
+})
+
+export default rootReducer
